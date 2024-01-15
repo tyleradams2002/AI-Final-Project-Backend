50f8abd97b6ee7cb06dd80ba63fc2056c57e1b94
tyleradams2002 <tylerjadams2002@gmail.com>
2024-01-14 21:42:01 -0500

    Updated Dependencies

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 const register = require("./controllers/register.js");
